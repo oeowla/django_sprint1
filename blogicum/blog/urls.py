83e532a3f501a741fb75f850a50660af702e2f86
Данил Цветков <danil18092007@icloud.com>
2025-02-28 02:31:01 +0500

    First_review

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('<int:id>/', views.post_detail, name = 'post_detail'),
+    path('<slug:category_slug>/', views.category_posts, name = 'category_posts'),
+]
